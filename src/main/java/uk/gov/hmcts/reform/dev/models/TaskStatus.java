cb9262b47adfb42a60334e586abad11c3c7f35b3
Mohammed Rafeeq <rafeeq.md@gmail.com>
2026-07-10 19:08:03 +0100

    Added Task entity and TaskStatus enum

---
+++ src/main/java/uk/gov/hmcts/reform/dev/models/TaskStatus.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.dev.models;
+
+/**
+ * The lifecycle states a caseworker task can be in.
+ */
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
